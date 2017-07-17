9bcbf6191085f03f8a2d52c9bccdb3560abb8b55
Namey <maporter104@gmail.com>
2017-07-17 17:50:07 -0500

    Initial Commit

---
+++ pageslinger/views.py
@@ -0,0 +1,5 @@
+from pageslinger import app
+
+@app.route('/')
+def hello():
+    return "placeholder"
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='pageslinger',
+    packages=['pageslinger'],
+    include_package_data=True,
+    install_requires=[
+        'pageslinger',
+    ],
+)
